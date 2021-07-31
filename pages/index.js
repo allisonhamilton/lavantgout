bfd30c9258171a5262e6729e52d59d11feed028b
Allison Hamilton <allisoneylh@gmail.com>
2021-07-31 08:04:13 -0400

    clean up of unecessary code and css for now

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>L'avant goût</title>
         <meta
@@ -14,12 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <Image src="/images/lavantgout.png" layout="fill" />
-        <h1 className={styles.title}>L'avant goût épicerie</h1>
-
-        <Image src="/public/images/lavantgout1.png" width={300} height={200} />
-        <Image src="/public/images/lavantgout2.svg" width={300} height={200} />
+        <h1>L'avant goût épicerie</h1>
       </main>
     </div>
   );
