286445b312e184a1d0b5d802a208bc0111ad7fda
Allison Hamilton <allisoneylh@gmail.com>
2021-07-31 08:08:19 -0400

    Issue: accents and apostraphe not detected > fixed

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>L'avant goût</title>
+        <title>{`L'avant goût`}</title>
         <meta
           name="description"
           content="L'avant goût une épicerie locaux à Montréal"
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main>
         <Image src="/images/lavantgout.png" layout="fill" />
-        <h1>L'avant goût épicerie</h1>
+        <h1>{`L'avant goût épicerie`}</h1>
       </main>
     </div>
   );
